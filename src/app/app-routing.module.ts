2e6f51827feb6d69e9d2e1ff55a6a25bd489907d
hoangm.ndm <hoangm.ndm@gmail.com>
2023-06-20 07:54:21 +0700

    create base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,48 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { ProductCreateComponent } from './product-create/product-create.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductUpdateComponent } from './product-update/product-update.component';
-import { ProductsListComponent } from './products-list/products-list.component';
-import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: 'products', component: ProductsListComponent },
-  { path: 'product-create', component: ProductCreateComponent },
-  { path: 'product-update/:id', component: ProductUpdateComponent },
-  { path: 'product-detail/:id', component: ProductDetailComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-];
-
-// const routes: Routes = [
-//   {
-//     path: '',
-//     component: LayoutClientComponent,
-//     children: [
-//       { path: '', component: HomeComponent },
-//       { path: 'about', component: AboutComponent },
-//       { path: 'login', component: LoginComponent },
-//       { path: 'register', component: RegisterComponent },
-//       { path: 'product/:id', component: ProductDetailComponent },
-//     ],
-//   },
-//   {
-//     path: 'admin',
-//     component: LayoutAdminComponent,
-//     children: [
-//       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-//       { path: 'dashboard', component: DashboardComponent },
-//       { path: 'products', component: ProductsComponent },
-//       { path: 'create-product', component: CreateProductComponent },
-//     ],
-//   },
-
-//   { path: '**', component: NotfoundComponent },
-// ];
-
+const routes: Routes = [];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list/products-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ProductCreateComponent } from './product-create/product-create.component';
-import { ProductUpdateComponent } from './product-update/product-update.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsListComponent,
-    ProductDetailComponent,
-    LoginComponent,
-    RegisterComponent,
-    ProductCreateComponent,
-    ProductUpdateComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
